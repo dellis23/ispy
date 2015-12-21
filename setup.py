57e4d4aec4e3f2fc1ca9e44f8a62679b40634d63
dan <dan@dans-MacBook-Pro.local>
2015-12-20 16:40:25 -0800

    Update version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='ispy',
     packages=['ispy'],
-    version='0.1.1',
+    version='0.1.2',
     license='GPLv2',
     description='A python script for monitoring the output of terminals and processes.',
     author='Daniel Ellis',
